6cbf791e5aeb46f3c531b42491232c05cb47cc7e
JingruiYu <yujingrui@sjtu.edu.cn>
2021-05-19 09:28:24 +0800

    init

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test/main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2021年05月19日 星期三 09时22分31秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
